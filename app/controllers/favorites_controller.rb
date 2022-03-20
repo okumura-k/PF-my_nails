bdca581495096ae319beb26a44f0e54f98c843dc
okumura-k <kh.0262.ok@gmail.com>
2022-03-20 12:31:16 +0000

    非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,15 @@
 class FavoritesController < ApplicationController
 
-  
-  
   def create
     @nail = Nail.find(params[:nail_id])
     favorite = current_user.favorites.new(nail_id: @nail.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @nail = Nail.find(params[:nail_id])
     favorite = current_user.favorites.find_by(nail_id: @nail.id)
     favorite.destroy
-    redirect_to request.referer
   end
-  
+
 end
--- app/controllers/nail_comments_controller.rb
+++ app/controllers/nail_comments_controller.rb
@@ -1,23 +1,22 @@
 class NailCommentsController < ApplicationController
-  
+
   def create
     @nail = Nail.find(params[:nail_id])
-    comment = current_user.nail_comments.new(nail_comment_params)
-    comment.nail_id = @nail.id
-    comment.save
-    redirect_to request.referer
-  end 
-  
+    @nail_comment = current_user.nail_comments.new(nail_comment_params)
+    @nail_comment.nail_id = @nail.id
+    @nail_comment.save
+  end
+
   def destroy
-    @nail = Nail.find(params[:nail_id])
-    NailComment.find(params[:id]).destroy
-    redirect_to request.referer
-  end 
-  
+    @nail_comment = NailComment.find(params[:id])
+    @nail_comment.destroy
+
+  end
+
   private
-  
+
   def nail_comment_params
     params.require(:nail_comment).permit(:comment)
-  end 
-  
+  end
+
 end
